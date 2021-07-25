2fb6cbbe4950a96c7286021ddefc39db73ab5ac9
yuevo <yuevo16@gmail.com>
2021-07-26 03:00:46 +0900

    screenshot setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,8 @@
 require 'capybara/rspec'
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    # driven_by :selenium_chrome_headless
-    driven_by :rack_test
+    driven_by :selenium_chrome_headless
+    # driven_by :rack_test
   end
   # config.after(:suite) do
   #   DatabaseCleaner.strategy = :transaction
